d74cc48f7f16385747aa8523be581666970bb2d3
Robert Planas <elmeunick9@gmail.com>
2018-04-12 01:00:22 +0200

    Hidato + TriHidato + Node + Tests

---
+++ src/main/java/domain/Hidato.java
@@ -0,0 +1,65 @@
+package domain;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public abstract class Hidato {
+    ArrayList<ArrayList<Node>> nodes;
+    private Map<Node, ArrayList<Node>> map = new HashMap<>();
+
+    public enum AdjacencyType { VERTEX, EDGE, BOTH };
+    protected AdjacencyType adjacency;
+
+    Hidato (ArrayList<ArrayList<Node>> data, AdjacencyType t) {
+        nodes = data;
+        adjacency = t;
+
+        for (int i = 1; i <= data.size(); i++)
+            for (int j = 1; j <= data.size(); j++) {
+                Node n = getNode(i, j);
+                if (!n.valid()) continue;
+                map.put(n, adjacentNodes(i, j));
+            }
+    }
+
+    protected abstract ArrayList<Node> adjacentNodes(int i, int j);
+    public ArrayList<Node> adjacentNodes(Node n) {
+        ArrayList<Node> t = new ArrayList<>(map.get(n));
+        t.remove(n);
+        return t;
+    }
+
+    public boolean isIndexBounded(int i, int j) {
+        return i > 0 && j > 0 && i <= nodes.size() && j <= nodes.get(i-1).size();
+    }
+
+    //Get a node from the matrix.
+    public Node getNode(int x, int y) throws IndexOutOfBoundsException {
+        return nodes.get(x-1).get(y-1);
+    }
+
+    //Get a node based on it's cell geometry and position.
+    public abstract Node getNodeByCoord(float i, float j);
+
+    public abstract void draw();
+
+    public void clear() {
+        for (ArrayList<Node> list : nodes) { for (Node n : list) n.clear(); }
+    }
+
+    public abstract Hidato copy();
+    public abstract Hidato copy(AdjacencyType t);
+
+    protected ArrayList<ArrayList<Node>> copyData() {
+        ArrayList<ArrayList<Node>> copy = new ArrayList(nodes.size());
+        for (ArrayList<Node> list : nodes) {
+            copy.add(new ArrayList<>());
+            for (Node x : list) {
+                copy.get(copy.size()-1).add(x.copy());
+            }
+        }
+        return copy;
+    }
+
+}
---
+++ src/test/java/domain/TriHidatoTest.java
@@ -0,0 +1,81 @@
+package domain;
+
+import org.junit.Test;
+import org.junit.Before;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+public class TriHidatoTest {
+    private TriHidato hidato;
+
+    @Before
+    public void before() {
+        ArrayList<ArrayList<Node>> data = new ArrayList<>();
+        String[][] matrix = {
+                {"#", "#", "#", "#", "#", "#", "#"},
+                {"#", "#", "#", "*", "#", "#", "#"},
+                {"#", "#", "?", "1", "?", "?", "?"},
+                {"#", "?", "?", "16","?", "?", "?"},
+                {"*", "?", "?", "7", "?", "?", "*"},
+        };
+
+        for (String[] list : matrix) {
+            data.add(new ArrayList<>());
+            for (String x : list) {
+                data.get(data.size() - 1).add(new Node(x));
+            }
+        }
+
+        hidato = new TriHidato(data, Hidato.AdjacencyType.EDGE);
+    }
+
+    @Test
+    public void copyTest() throws Node.InvalidTypeException {
+        Hidato c = hidato.copy(Hidato.AdjacencyType.VERTEX);
+        c.getNode(4, 2).setValue(8);
+        hidato.getNode(4, 2).setValue(9);
+        assertEquals(c.getNode(1,1).getType(), Node.Type.invisible);
+        assertEquals(c.getNode(5,1).getType(), Node.Type.block);
+        assertEquals(c.getNode(5,7).getType(), Node.Type.block);
+        assertEquals(c.getNode(3,4).getType(), Node.Type.fixed);
+        assertEquals(c.getNode(3,4).getValue(), 1);
+        assertEquals(c.getNode(4,2).getValue(), 8);
+        assertEquals(hidato.getNode(4,2).getValue(), 9);
+    }
+
+    @Test
+    public void codificationIsGood() throws Node.InvalidTypeException {
+        hidato.getNode(4, 2).setValue(8);
+        assertEquals(hidato.getNode(1,1).getType(), Node.Type.invisible);
+        assertEquals(hidato.getNode(5,1).getType(), Node.Type.block);
+        assertEquals(hidato.getNode(5,7).getType(), Node.Type.block);
+        assertEquals(hidato.getNode(3,4).getType(), Node.Type.fixed);
+        assertEquals(hidato.getNode(3,4).getValue(), 1);
+        assertEquals(hidato.getNode(4,2).getValue(), 8);
+    }
+
+    @Test
+    public void adjacencyTest() throws Node.InvalidTypeException {
+        //Test by EDGE
+        Node a = hidato.getNode(5, 4);
+        assertEquals(a.getValue(), 7);
+
+        Node b = hidato.getNode(4, 3);
+        b.setValue(3);
+
+        ArrayList<Node> list = hidato.adjacentNodes(hidato.getNode(4, 4));
+        assertTrue(list.contains(a));
+        assertTrue(list.contains(b));
+        assertEquals(list.get(list.indexOf(b)).getValue(), 3);
+
+        //Test by VERTEX
+        Hidato c = hidato.copy(Hidato.AdjacencyType.VERTEX);
+        a = hidato.getNode(5, 4);
+        b = hidato.getNode(3, 4);
+        list = hidato.adjacentNodes(hidato.getNode(4, 4));
+        assertTrue(list.contains(a));
+        assertTrue(list.toString().contains("?"));
+    }
+}
