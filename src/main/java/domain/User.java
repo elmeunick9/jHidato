9a595fadbeb3a0d97f713076b8cad6c918fd82c7
OscarOchoaMartin <oscarochoa21@gmail.com>
2018-04-19 20:48:05 +0200

    Added src/main/java/domain/User.java with constructor and getters

---
+++ src/main/java/domain/User.java
@@ -0,0 +1,19 @@
+package domain;
+
+public class User {
+    private String name;
+    private int totalGames;
+
+    public User(String name){
+        this.name = name;
+        this.totalGames = 0;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public int getTotalGames(){
+        return this.totalGames;
+    }
+}
