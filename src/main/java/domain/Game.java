4f72254d6d47c545b5e90856a86a0682cc63d24d
OscarOchoaMartin <oscarochoa21@gmail.com>
2018-05-01 13:48:15 +0200

    Added basic implementation of Game and Tests

--- src/main/java/domain/Game.java
+++ src/main/java/domain/Game.java
@@ -1,4 +1,80 @@
 package domain;
 
+
 public class Game {
+    public enum Difficulty { EASY, MEDIUM, HARD, CUSTOM }
+    private int score;
+    private Difficulty dif;
+    private Hidato h;
+    private String filename;
+    private User user;
+    //time played;
+    Game(Difficulty d, User u) {
+        score = 0;
+        GeneratorStub g = new GeneratorStub(d);
+        h = g.getHidato();
+        dif = d;
+        filename = g.getHashedFilename();
+        user = u;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public User getPlayer() {
+        return user;
+    }
+
+    public String getFilename() {
+        return filename;
+    }
+
+    public Difficulty getDif() {
+        return dif;
+    }
+
+    public int getValue(int x, int y) throws Node.InvalidTypeException{
+        return h.getNode(x,y).getValue();
+    }
+
+    /*Add or substract the score with minimum of 0*/
+    private void changeScore(int s) {
+        if(s > 0  || (this.score + s) >= 0)
+            this.score += s;
+        else if(s < 0) {
+            this.score = 0;
+        }
+    }
+
+    private boolean moveIsvalid(Node n) {
+        boolean ret = false;
+        if(n.editable())
+            ret = true;
+        return ret;
+    }
+
+    public void setDif(Difficulty d) {
+        this.dif = d;
+    }
+
+    public void move(int x, int y, int value) throws Node.InvalidTypeException {
+        if(moveIsvalid(this.h.getNode(x, y))) {
+            this.h.getNode(x, y).setValue(value);
+            changeScore(1);
+        } else {
+            changeScore(-1);
+        }
+
+    }
+
+    /*Save the stats of the game when user pause or leave the game*/
+    public void saveGame() {
+
+    }
+
+    /*Refresh stats from user and ranking when a game is over*/
+    public void finishGame() {
+
+    }
 }
--- src/test/java/domain/GameTest.java
+++ src/test/java/domain/GameTest.java
@@ -1,4 +1,39 @@
 package domain;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
 public class GameTest {
+    private Game game;
+    private User user;
+
+    @Before
+    public void before() {
+        user = new User("test");
+        game = new Game(Game.Difficulty.EASY, user);
+    }
+
+    @Test
+    public void initialState() {
+        assertEquals(0, game.getScore());
+        assertEquals(user, game.getPlayer());
+        assertEquals(Game.Difficulty.EASY, game.getDif());
+    }
+
+    @Test
+    public void validMove() throws Node.InvalidTypeException {
+        game.move(4, 2, 9);
+        assertEquals(1, game.getScore());
+        assertEquals(9, game.getValue(4,2));
+    }
+
+    @Test
+    public void invalidMove() throws Node.InvalidTypeException {
+        game.move(4, 2, 9);
+        assertEquals(1, game.getScore());
+        game.move(3,4,1);
+        assertEquals(0, game.getScore());
+    }
 }
