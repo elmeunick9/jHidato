942aebd837174388d961fcb2a391c137539e36c1
Robert Planas <elmeunick9@gmail.com>
2018-06-02 14:51:57 +0200

    menu

--- src/main/java/presentation/MainWindow.java
+++ src/main/java/presentation/MainWindow.java
@@ -10,15 +10,26 @@ public class MainWindow {
     private JPanel panelContent = new JPanel();
     private JMenuBar menuMain = new JMenuBar();
     private JMenu menuFile = new JMenu("File");
+    private JMenuItem menuitemNewGame = new JMenuItem("New Game");
+    private JMenuItem menuitemLoadgame = new JMenuItem("Load Game");
+    private JMenuItem menuitemSavegame = new JMenuItem("Save Game");
+    private JMenuItem menuitemSaveAs = new JMenuItem("Save Game As Template...");
     private JMenuItem menuitemQuit = new JMenuItem("Quit");
+    private JMenu menuRanking = new JMenu("Ranking");
+    private JMenu menuHelp = new JMenu("Help");
+    private JMenuItem menuitemAbout = new JMenuItem("About");
+    private JMenuItem menuitemManual = new JMenuItem("Manual");
+
     private JOptionPane loginDialog = new JOptionPane();
 
     public MainWindow(CtrlPresentation cPres) {
         ctrlPresentation = cPres;
-        init();
+        initView();
+        initActions();
+        askUser();
     }
 
-    private void init() {
+    private void initView() {
         // Frame
         frame.setMinimumSize(new Dimension(700,700));
         frame.setPreferredSize(frame.getMinimumSize());
@@ -27,14 +38,28 @@ public class MainWindow {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         //Menu
+        menuFile.add(menuitemNewGame);
+        menuFile.add(menuitemLoadgame);
+        menuFile.add(menuitemSavegame);
+        menuFile.add(menuitemSaveAs);
         menuFile.add(menuitemQuit);
         menuMain.add(menuFile);
+        menuMain.add(menuRanking);
+        menuHelp.add(menuitemManual);
+        menuHelp.add(menuitemAbout);
+        menuMain.add(menuHelp);
         frame.setJMenuBar(menuMain);
-
-        menuitemQuit.addActionListener(e -> System.exit(0));
 
         //Main Content
 
+
+    }
+
+    private void initActions() {
+        menuitemQuit.addActionListener(e -> System.exit(0));
+    }
+
+    private void askUser() {
         //Show Login
         String usrname = "";
         while (usrname == "") {
